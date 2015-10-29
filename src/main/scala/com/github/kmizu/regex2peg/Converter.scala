11f7520085cce48f10d1310e7742acb1aad85312
Kota Mizushima <mizukota@gmail.com>
2015-10-29 23:31:07 +0900

    Improve code quality.

--- src/main/scala/com/github/kmizu/regex2peg/Converter.scala
+++ src/main/scala/com/github/kmizu/regex2peg/Converter.scala
@@ -8,7 +8,7 @@ object Converter {
   def convert(exp: REG.Exp): (NonTerminal, mutable.Map[NonTerminal, PEG.Exp]) = {
     val start = NonTerminal("S")
     val rules = mutable.Map[NonTerminal, PEG.Exp]()
-    val generator = Generator("A", 0)
+    val generator = Generator("A")
     def C(exp: REG.Exp, k: PEG.Exp): PEG.Exp = exp match {
       case REG.Eps => PEG.Cat(PEG.Eps, k)
       case REG.Chr(ch) => PEG.Cat(PEG.Chr(ch), k)
--- src/main/scala/com/github/kmizu/regex2peg/Generator.scala
+++ src/main/scala/com/github/kmizu/regex2peg/Generator.scala
@@ -1,6 +1,8 @@
 package com.github.kmizu.regex2peg
 
-case class Generator(prefix: String, var index: Int) {
+case class Generator(prefix: String) {
+  private var index: Int = 0
+
   def symbol(): String = {
     index += 1
     prefix + index
